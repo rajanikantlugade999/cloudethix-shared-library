1b05118e46c516c59d0b610e8a6d4f5b75a1eec3
rajanikantlugade999 <39275511+rajanikantlugade999@users.noreply.github.com>
2024-07-24 16:21:07 +0530

    Update DockerDeliveryPipeline.groovy

--- vars/DockerDeliveryPipeline.groovy
+++ vars/DockerDeliveryPipeline.groovy
@@ -1,8 +1,8 @@
 def call(body) {
-def config = [:]
-body.resolveStrategy = Closure.DELEGATE_FIRST
-body.delegate = config
-body()
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
         pipeline {
         agent any
     
